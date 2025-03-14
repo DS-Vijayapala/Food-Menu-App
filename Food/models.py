aabf4110e40b46b3d9cc819d8dff2e6577efab76
Dineth <dinethfiverr23@gmail.com>
2025-03-14 16:34:12 +0530

    Adding Image Field To Model , Adding Image , Desigining The Detail View

--- Food/models.py
+++ Food/models.py
@@ -12,3 +12,6 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField(default=0)
+    item_image = models.CharField(
+        max_length=500,
+        default='https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_1280.jpg')
