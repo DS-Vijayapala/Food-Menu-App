58db862d5bbb4c242be0845ab59a11ffd0529cca
Dineth <dinethfiverr23@gmail.com>
2025-03-14 14:37:16 +0530

    Creating The Detail View , Completing The Detail View, Removing Hardcoded URLs And Namespacing

--- Food/urls.py
+++ Food/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 from . import views
 
 
+# for Namespacing
+app_name = 'food'
+
+
 urlpatterns = [
+    # food/
     path('', views.index, name='index'),
+
+    # food/1/
+    path('<int:item_id>/', views.detail, name='Detail'),
     path('item/', views.item, name='item'),
 
 ]
--- Food/views.py
+++ Food/views.py
@@ -1,6 +1,6 @@
 """ This file is used to create the views for the food app. """
 
-from django.template import loader
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
@@ -19,4 +19,17 @@ def index(request):
 
 
 def item(request):
+    """ This function is used to display the items. """
+
     return HttpResponse("<h1>Hello, world. You're at the polls item.</h1>")
+
+
+def detail(request, item_id):
+    """ This function is used to display the details of the item. """
+
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'food/detail.html', context)
