5fdc61c7b154cca7bc6de15d8b915d1bf7a9403e
Dineth <dinethfiverr23@gmail.com>
2025-03-17 15:15:43 +0530

    Adding The User Profile Picture

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,8 @@ from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 from Users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -29,3 +31,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profilepage, name='profile'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
