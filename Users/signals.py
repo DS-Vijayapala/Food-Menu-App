a5568d72e21d2fee5467afc8b079ae01ea99424e
Dineth <dinethfiverr23@gmail.com>
2025-03-18 11:19:07 +0530

    Automate Profile Creation Process Using Django Signals

---
+++ Users/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def build_profile(sender, instance, created, **kwargs):
+    """ Create a profile for each new user """
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    """ Save the profile for each new user """
+    instance.profile.save()
